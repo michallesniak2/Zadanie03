83620e6506873c83f1d5095b1a0c987a0b4dd8d0
Michal Lesniak <michallesniak2@gmail.com>
2018-05-26 19:22:39 +0200

    Second commit Survey

--- 2_Survey/main.py
+++ 2_Survey/main.py
@@ -1,1 +1,11 @@
-# Your code here...
+wiek = int(input("Ile masz lat? "))
+if wiek >= 18:
+    odp_1_1 = input("Pijesz? (y/n) ")
+    print(odp_1_1)
+    odp_1_2 = input("Palisz? (y/n) ")
+    print(odp_1_2)
+else:
+    odp_2_1 = input("Dobrze się uczysz? (y/n) ")
+    print(odp_2_1)
+    odp_2_2 = input("Chcesz cukierka? (y/n) ")
+    print(odp_2_2)
