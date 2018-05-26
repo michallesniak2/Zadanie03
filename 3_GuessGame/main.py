16a7f1176189c67a97f0369a806c51b8e86206e4
Michal Lesniak <michallesniak2@gmail.com>
2018-05-26 20:07:29 +0200

    Third commit Game

--- 3_GuessGame/main.py
+++ 3_GuessGame/main.py
@@ -1,1 +1,20 @@
-# Your code here...
+import random
+
+flaga = False
+moj_licznik = random.randint(1,100)
+print(moj_licznik)
+licznik = 0
+
+while flaga == False:
+    licznik += 1
+    wartosc = int(input("Podaj liczbę od 1 do 100: "))
+    if wartosc > moj_licznik:
+        print("Liczba za duża")
+    elif wartosc < moj_licznik:
+        print("Liczba za mała")
+    else:
+        print("BINGO")
+        flaga = True
+
+
+print("Udało ci się zgadnąć po",licznik,"próbach" )
