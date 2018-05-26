359d4ef8a06b9ee4666b62855e17cccc38f8de37
Michal Lesniak <michallesniak2@gmail.com>
2018-05-26 19:35:07 +0200

    Second commit Survey

--- 2_Survey/main.py
+++ 2_Survey/main.py
@@ -1,9 +1,13 @@
+imie = input("Jak masz na imię? ")
+nazwisko = input("Jak masz na nazwisko? ")
 wiek = int(input("Ile masz lat? "))
+
 if wiek >= 18:
-    odp_1_1 = input("Pijesz? (y/n) ")
+    odp_1_1 = input("Pijesz (y/n)? ")
     print(odp_1_1)
-    odp_1_2 = input("Palisz? (y/n) ")
+    odp_1_2 = input("Palisz (y/n)? ")
     print(odp_1_2)
+    print(imie,nazwisko,"lat",wiek,"pije",odp_1_1,"pali",odp_1_2)
 else:
     odp_2_1 = input("Dobrze się uczysz? (y/n) ")
     print(odp_2_1)
